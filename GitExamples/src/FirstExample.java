473c67f495507662cd11a7302f0a37007b73533e
ronithroni <ronithroni>
2018-01-11 19:45:45 +0530

    modification

--- GitExamples/src/FirstExample.java
+++ GitExamples/src/FirstExample.java
@@ -3,6 +3,6 @@ public class FirstExample {
 
 	public static void main(String[] args) {
 		System.out.println("First git example completed");
-		System.out.println("");
+		System.out.println("Modification at progess");
 	}
 }
